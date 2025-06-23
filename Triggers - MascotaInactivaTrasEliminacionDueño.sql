1dbbf8357b314926bc5f0492023314eda4095199
Martín <martin.sueldo@alumnos.frgp.utn.edu.ar>
2025-06-23 00:24:53 -0300

    Update Triggers - MascotaInactivaTrasEliminacionDueño.sql

--- Triggers - MascotaInactivaTrasEliminacionDueño.sql
+++ Triggers - MascotaInactivaTrasEliminacionDueño.sql
@@ -7,14 +7,14 @@ AS
 BEGIN
     -- Solo cuando el dueño fue inactivado
     IF EXISTS (
-        SELECT 1 FROM INSERTED i 
-        JOIN DELETED d ON i.IDDueño = d.IDDueño
-        WHERE i.Activo = 0 AND d.Activo = 1
+        SELECT 1 FROM INSERTED AS I 
+        INNER JOIN DELETED AS D ON I.IDDueño = D.IDDueño
+        WHERE I.Activo = 0 AND D.Activo = 1
     )
     BEGIN
         UPDATE M
         SET Activo = 0
-        FROM Mascotas M
-        JOIN INSERTED i ON M.IDDueño = i.IDDueño
+        FROM Mascotas AS M
+        INNER JOIN INSERTED AS I ON M.IDDueño = I.IDDueño
     END
 END;
